9b6639c43d83c17351caaa925fb5eb622f54bf6d
TereTorres <teretorres@outlook.com.ar>
2022-07-19 21:32:43 -0300

    educacionYexperiencia

--- Front End/ProyectoFinal/src/app/app.module.ts
+++ Front End/ProyectoFinal/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
