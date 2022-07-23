90b4fbca3601b1d267daa02b9a40ba41a577a440
TereTorres <teretorres@outlook.com.ar>
2022-07-23 15:37:23 -0300

    conexion con servidor

---
+++ Back End/TereTorres/src/main/java/com/portfolio/TereTorres/TereTorresApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.TereTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TereTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TereTorresApplication.class, args);
+	}
+
+}
